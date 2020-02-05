be92970695c4b081373df5ff3344e1103e17bf19
Louis <louisbarrettevanasse@yahoo.ca>
2020-02-05 17:33:31 -0500

    try to fix herok

--- PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/PetShelterApplication.java
+++ PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/PetShelterApplication.java
@@ -14,6 +14,7 @@ public class PetShelterApplication {
 package ca.mcgill.ecse321.petshelter;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.boot.SpringApplication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @RestController
 @SpringBootApplication
+@EnableJpaRepositories
 public class PetShelterApplication {
 
   public static void main(String[] args) {
