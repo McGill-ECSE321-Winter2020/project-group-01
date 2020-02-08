bfb349c2548c53926b3cc98587184e3ea0438a43
chris <christopher.boustros@mail.mcgill.ca>
2020-02-08 01:48:30 -0500

    Set up PetShelterBackendRepository

--- PetShelter-Backend/src/test/java/ca/mcgill/ecse321/petshelter/PetShelterApplicationTests.java
+++ PetShelter-Backend/src/test/java/ca/mcgill/ecse321/petshelter/PetShelterApplicationTests.java
@@ -2,7 +2,10 @@ package ca.mcgill.ecse321.petshelter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class PetShelterApplicationTests {
 
