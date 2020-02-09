b357e4af9456207972366b78b60fb7c33046b792
Ding Ma <ding.ma@mail.mcgill.ca>
2020-02-09 11:26:24 -0500

    making progress in the tests

--- PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/repository/DonationRepository.java
+++ PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/repository/DonationRepository.java
@@ -2,11 +2,14 @@ package ca.mcgill.ecse321.petshelter.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import ca.mcgill.ecse321.petshelter.model.Donation;
+import ca.mcgill.ecse321.petshelter.model.User;
 
 /**
  * @author louis
  *
  */
 public interface DonationRepository extends JpaRepository<Donation, Long>{
+
+	Donation findDonationByUserAndAmount(User user, double amount);
 
 }
