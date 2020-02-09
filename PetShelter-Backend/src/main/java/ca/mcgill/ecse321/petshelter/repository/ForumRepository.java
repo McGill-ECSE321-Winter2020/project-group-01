3d65edf857588f1aaa7158c329c5d3e4cced7044
Ding Ma <ding.ma@mail.mcgill.ca>
2020-02-09 11:54:54 -0500

    forum creating is breaking the tests

--- PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/repository/ForumRepository.java
+++ PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/repository/ForumRepository.java
@@ -1,12 +1,20 @@
 package ca.mcgill.ecse321.petshelter.repository;
 
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import ca.mcgill.ecse321.petshelter.model.Comment;
 import ca.mcgill.ecse321.petshelter.model.Forum;
+import ca.mcgill.ecse321.petshelter.model.User;
 
 /**
  * @author louis
  *
  */
 public interface ForumRepository extends JpaRepository<Forum, Long>{
+
+	//TODO: this is breaking the tests
+//	Forum findForumByUserSetAndCommentSet(Set<User> userSet, Set<Comment> commentSet);
 
 }
