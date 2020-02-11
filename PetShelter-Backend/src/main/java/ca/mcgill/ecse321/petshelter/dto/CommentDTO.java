4b217333049fac1d68d45675244821889b6c09df
Mat Bis <mathieu.bissonnette2@mail.mcgill.ca>
2020-02-11 18:16:02 -0500

    Add the Comment Data Transfer Object.

---
+++ PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/dto/CommentDTO.java
@@ -0,0 +1,81 @@
+package ca.mcgill.ecse321.petshelter.dto;
+
+import java.sql.Date;
+import java.sql.Time;
+
+public class CommentDTO {
+	private Date datePosted;
+
+	public void setDatePosted(Date value) {
+		this.datePosted = value;
+	}
+
+	public Date getDatePosted() {
+		return this.datePosted;
+	}
+
+	private long id;
+
+	public void setId(long value) {
+		this.id = value;
+	}
+
+	public long getId() {
+		return this.id;
+	}
+
+	private String text;
+
+	public void setText(String value) {
+		this.text = value;
+	}
+
+	public String getText() {
+		return this.text;
+	}
+    
+    private Time time;
+    
+    public void setTime(Time value) {
+        this.time = value;
+    }
+    
+    public Time getTime() {
+        return this.time;
+    }
+    
+    /**
+     * Author of the comment.
+     */
+    
+    private UserDTO user;
+    
+    /**
+     * Return the author of the comment.
+     * @return The author.
+     */
+    
+    public UserDTO getUser() {
+        return this.user;
+    }
+    
+    /**
+     * Set the author of the comment.
+     * @param user The new author.
+     */
+    
+    public void setUser(UserDTO user) {
+        this.user = user;
+    }
+    
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "datePosted=" + datePosted +
+                ", id=" + id +
+                ", text='" + text + '\'' +
+                ", time=" + time +
+                ", user=" + user +
+                '}';
+    }
+}
