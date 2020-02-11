8e32b183a28de8275598d7fddc9b7a87a9e92b63
Louis <louisbarrettevanasse@yahoo.ca>
2020-02-11 10:29:57 -0500

    added dtos

---
+++ PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/dto/UserDTO.java
@@ -0,0 +1,87 @@
+package ca.mcgill.ecse321.petshelter.dto;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
+import ca.mcgill.ecse321.petshelter.model.UserType;
+
+public class UserDTO {
+	
+	@NotNull
+	@Email
+	private String email;
+	@NotNull
+	private String username;
+	private String password;
+	private UserType userType;
+	private byte[] picture;
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return the userType
+	 */
+	public UserType getUserType() {
+		return userType;
+	}
+
+	/**
+	 * @param userType the userType to set
+	 */
+	public void setUserType(UserType userType) {
+		this.userType = userType;
+	}
+
+	/**
+	 * @return the picture
+	 */
+	public byte[] getPicture() {
+		return picture;
+	}
+
+	/**
+	 * @param picture the picture to set
+	 */
+	public void setPicture(byte[] picture) {
+		this.picture = picture;
+	}
+}
