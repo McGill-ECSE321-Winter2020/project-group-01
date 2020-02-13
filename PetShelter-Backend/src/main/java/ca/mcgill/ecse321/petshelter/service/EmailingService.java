9d4ee41b17e3dd5dc5516aefad6e9b0b5f570319
Ding Ma <ding.ma@mail.mcgill.ca>
2020-02-13 17:13:30 -0500

    refactored user creation EmailingServices

--- PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/service/EmailingService.java
+++ PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/service/EmailingService.java
@@ -1,39 +1,66 @@
 package ca.mcgill.ecse321.petshelter.service;
 
-import ca.mcgill.ecse321.petshelter.dto.DonationDTO;
-import ca.mcgill.ecse321.petshelter.model.User;
 import ca.mcgill.ecse321.petshelter.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.sql.Date;
+import java.sql.Time;
 import java.text.NumberFormat;
 
 @Service
 public class EmailingService {
+    
+    @Qualifier("mailSender")
     @Autowired
     private JavaMailSender javaMailSender;
     @Autowired
     private UserRepository userRepository;
     
+    private String serviceEmail = "${spring.mail.username}";
+    
+    @Value("${baseurl}")
+    private String url;
+    
     /**
      * Sends email confirmation for donation
      *
-     * @param donationDTO donation instance of user
+     * @param email    user's email
+     * @param username user's username
+     * @param amount   user's donation amount
+     * @param time     donation time
+     * @param date     donation date
      */
-    public void donationConfirmation(DonationDTO donationDTO) {
-        User donor = userRepository.findUserByUserName(donationDTO.getUsername());
+    public void donationConfirmationEmail(String email, String username, double amount, Time time, Date date) {
         NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
         SimpleMailMessage msg = new SimpleMailMessage();
-        msg.setFrom("${spring.mail.username}");
-        msg.setTo(donor.getEmail());
+        msg.setFrom(serviceEmail);
+        msg.setTo(email);
         
         msg.setSubject("Donation Confirmation");
-        msg.setText("Hey " + donationDTO.getUsername() + " , \n\n" + "Thank you for your donation. We have successfully processed it at " +
-                donationDTO.getTime() + " EST " + donationDTO.getDate() + " for an amount of: " + currencyFormat.format(donationDTO.getAmount()) + "\n\nYours, \nPetShelter");
+        msg.setText("Hey " + username + " , \n\n" + "Thank you for your donation. We have successfully processed it at " +
+                time + " EST " + date + " for an amount of: " + currencyFormat.format(amount) + "\n\nYours, \nPetShelter");
         
         javaMailSender.send(msg);
     }
     
+    /**
+     * Sends email to confirm user's email address
+     *
+     * @param email    user's email
+     * @param username user's username
+     * @param token    randomly generated token for confirmation
+     */
+    public void userCreationEmail(String email, String username, String token) {
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setFrom(serviceEmail);
+        msg.setTo(email);
+        msg.setSubject("Account Creation Verification for " + username);
+        msg.setText("Hey " + username + " ,\n\n Please confirm your email here: " + url + "registrationConfirmation?token=" + token + "\n\nYours, \nPetShelter");
+        javaMailSender.send(msg);
+    }
 }
