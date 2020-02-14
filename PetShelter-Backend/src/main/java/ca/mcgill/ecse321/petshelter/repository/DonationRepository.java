cbb126d5db83f605c26b386c6b34fe4ecb979850
chris <christopher.boustros@mail.mcgill.ca>
2020-02-14 17:01:04 -0500

    Added @RepositoryRestResource annotation to repository interfaces as per section 2.8.3 part 4 of tutorial notes 🤓

--- PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/repository/DonationRepository.java
+++ PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/repository/DonationRepository.java
@@ -1,6 +1,8 @@
 package ca.mcgill.ecse321.petshelter.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
 import ca.mcgill.ecse321.petshelter.model.Donation;
 import ca.mcgill.ecse321.petshelter.model.User;
 
@@ -8,6 +10,8 @@ import ca.mcgill.ecse321.petshelter.model.User;
  * @author louis
  *
  */
+//REST endpoint specification
+@RepositoryRestResource(collectionResourceRel = "donation_data", path = "donation_data")
 public interface DonationRepository extends JpaRepository<Donation, Long>{
 
 	Donation findDonationByUserAndAmount(User user, double amount);
