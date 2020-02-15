f1a5246df258e5f492a49fe861801f72bd1f503e
Louis <louisbarrettevanasse@yahoo.ca>
2020-02-15 15:02:33 -0500

    Created @ValidPassword annotation for password strength
    du pur genie

---
+++ PetShelter-Backend/src/main/java/ca/mcgill/ca/ecse321/petshelter/passwordvalidator/PasswordConstraintValidator.java
@@ -0,0 +1,43 @@
+package ca.mcgill.ca.ecse321.petshelter.passwordvalidator;
+
+import java.util.Arrays;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.passay.DigitCharacterRule;
+import org.passay.LengthRule;
+import org.passay.PasswordData;
+import org.passay.PasswordValidator;
+import org.passay.RuleResult;
+import org.passay.SpecialCharacterRule;
+import org.passay.UppercaseCharacterRule;
+
+import com.google.common.base.Joiner;
+
+/**
+ * Class that validates a password to be strong. Taken from Baeldung tutorials.
+ * @author louis
+ *
+ */
+public class PasswordConstraintValidator implements ConstraintValidator<ValidPassword, String> {
+
+	@Override
+	public void initialize(ValidPassword arg0) {
+	}
+
+	@Override
+	public boolean isValid(String password, ConstraintValidatorContext context) {
+		PasswordValidator validator = new PasswordValidator(Arrays.asList(new LengthRule(8, 30),
+				new UppercaseCharacterRule(1), new DigitCharacterRule(1), new SpecialCharacterRule(1)));
+
+		RuleResult result = validator.validate(new PasswordData(password));
+		if (result.isValid()) {
+			return true;
+		}
+		context.disableDefaultConstraintViolation();
+		context.buildConstraintViolationWithTemplate(Joiner.on(",").join(validator.getMessages(result)))
+				.addConstraintViolation();
+		return false;
+	}
+}
