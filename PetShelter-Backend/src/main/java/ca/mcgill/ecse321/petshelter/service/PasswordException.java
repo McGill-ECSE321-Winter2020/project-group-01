16aaa0fc5163fbf04d008cedd9e4dec0c84e5381
Ding Ma <ding.ma@mail.mcgill.ca>
2020-02-15 13:30:50 -0500

    usertests set up is done, waiting on louis for the services to be completed

---
+++ PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/service/PasswordException.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.petshelter.service;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.CONFLICT)
+public class PasswordException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+    
+    public PasswordException(String msg) {
+        super(msg);
+    }
+}
--- PetShelter-Backend/src/test/java/ca/mcgill/ecse321/petshelter/service/TestUserService.java
+++ PetShelter-Backend/src/test/java/ca/mcgill/ecse321/petshelter/service/TestUserService.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.validation.constraints.Email;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -152,22 +151,57 @@ public class TestUserService {
     
     @Test
     public void testChangeUserPassword(){
+        UserDTO userDTO = new UserDTO();
     
+        String email = "myEmail@gmail.com";
+        String password = "myPassword123";
+        String username = "myUsername";
+        String newPassword = "newPassword123";
+        UserType userType = USER;
+    
+        userDTO.setEmail(email);
+        userDTO.setPassword(password);
+        userDTO.setUsername(username);
+        userDTO.setUserType(userType);
+    
+        //todo
     }
     
     @Test
-    public void testChangeUserPasswordToNull(){
+    public void testChangeUserPasswordToNull() {
+        UserDTO userDTO = new UserDTO();
     
+        String email = "myEmail@gmail.com";
+        String password = "myPassword123";
+        String username = "myUsername";
+        String newPassword = null;
+        UserType userType = USER;
+    
+        userDTO.setEmail(email);
+        userDTO.setPassword(password);
+        userDTO.setUsername(username);
+        userDTO.setUserType(userType);
+    
+        //todo
     }
     
-    @Test
-    public void testChangeUserPasswordToInvalid(){
-    
-    }
     
     @Test
-    public void testChangeInvalidPassword(){
+    public void testChangeInvalidPassword() {
+        UserDTO userDTO = new UserDTO();
     
+        String email = "myEmail@gmail.com";
+        String password = "myPassword123";
+        String username = "myUsername";
+        String newPassword = "123";
+        UserType userType = USER;
+    
+        userDTO.setEmail(email);
+        userDTO.setPassword(password);
+        userDTO.setUsername(username);
+        userDTO.setUserType(userType);
+    
+        //todo
     }
     
 }
