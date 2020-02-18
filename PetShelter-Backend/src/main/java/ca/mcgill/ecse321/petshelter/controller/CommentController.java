ea430582d0a152c1ea0ac9cac0e584afc71eea92
Mat Bis <mathieu.bissonnette2@mail.mcgill.ca>
2020-02-17 21:33:46 -0500

    Add annotation & DTO convertion in forum/comment.
    Had to change the visibility of userToDto to package and make it static
    to be able to access it from the forum and comment controller classes.

--- PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/controller/CommentController.java
+++ PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/controller/CommentController.java
@@ -1,5 +1,38 @@
 package ca.mcgill.ecse321.petshelter.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ca.mcgill.ecse321.petshelter.dto.CommentDTO;
+import ca.mcgill.ecse321.petshelter.model.Comment;
+import ca.mcgill.ecse321.petshelter.repository.CommentRepository;
+import ca.mcgill.ecse321.petshelter.repository.UserRepository;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api/comment")
 public class CommentController {
+	
+	@Autowired
+	CommentRepository commentRepository;
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	/**
+	 * Convert a comment entity to a comment DTO.
+	 * @param comment The comment to convert.
+	 * @return A comment DTO.
+	 */
+	static CommentDTO commentToDto(Comment comment) {
+		 CommentDTO commentDTO = new CommentDTO();
+		 commentDTO.setDatePosted(comment.getDatePosted());
+		 commentDTO.setId(comment.getId());
+		 commentDTO.setText(comment.getText());
+		 commentDTO.setUser(UserController.userToDto(comment.getUser()));
+		 return commentDTO;
+	}
 
 }
