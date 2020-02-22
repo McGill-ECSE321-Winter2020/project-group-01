6ee71eef983e5184b7dc6abbd551af7b93682cc9
Mat Bis <mathieu.bissonnette2@mail.mcgill.ca>
2020-02-22 01:42:22 -0500

    Add implementation of REST controller methods.

--- PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/controller/CommentController.java
+++ PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/controller/CommentController.java
@@ -1,14 +1,24 @@
 package ca.mcgill.ecse321.petshelter.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import ca.mcgill.ecse321.petshelter.dto.CommentDTO;
 import ca.mcgill.ecse321.petshelter.model.Comment;
 import ca.mcgill.ecse321.petshelter.repository.CommentRepository;
 import ca.mcgill.ecse321.petshelter.repository.UserRepository;
+import ca.mcgill.ecse321.petshelter.service.CommentService;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -21,8 +31,33 @@ public class CommentController {
 	@Autowired
 	UserRepository userRepository;
 	
-	// The getting of comments is handled by the forum controller.
-
+	@Autowired
+	CommentService commentService;
+	
+	@GetMapping("/all")
+	public List<CommentDTO> getAllComments() {
+		return commentService.getComments().stream()
+				.map(CommentController::commentToDto)
+				.collect(Collectors.toList());
+	}
+	
+	@GetMapping("/{username}")
+	public List<CommentDTO> getUserComments(@PathVariable String username) {
+		return commentService.getCommentsByUser(userRepository.findUserByUserName(username).getId())
+				.stream().map(CommentController::commentToDto)
+				.collect(Collectors.toList());
+	}
+	
+	@PostMapping("/")
+	public ResponseEntity<?> createComment(@PathVariable String username) {
+		return null; //TODO
+	}
+	
+	@PutMapping("/")
+	public ResponseEntity<?> updateComment(@RequestBody CommentDTO commentDTO) {
+		return null; //TODO
+	}
+	
 	/**
 	 * Convert a comment entity to a comment DTO.
 	 * 
