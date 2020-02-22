bf8abf528d5ab4c0b74378617f785aa5cbeed278
Louis <louisbarrettevanasse@yahoo.ca>
2020-02-22 01:46:00 -0500

    Update CommentController.java

--- PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/controller/CommentController.java
+++ PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/controller/CommentController.java
@@ -1,26 +1,18 @@
 package ca.mcgill.ecse321.petshelter.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.mail.MailException;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ca.mcgill.ecse321.petshelter.dto.CommentDTO;
-import ca.mcgill.ecse321.petshelter.dto.UserDTO;
 import ca.mcgill.ecse321.petshelter.model.Comment;
-import ca.mcgill.ecse321.petshelter.model.User;
 import ca.mcgill.ecse321.petshelter.repository.CommentRepository;
 import ca.mcgill.ecse321.petshelter.repository.UserRepository;
-import ca.mcgill.ecse321.petshelter.service.RegisterException;
 
 @CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/api/comment")
+@RequestMapping("/api/forum")
 public class CommentController {
 
 	@Autowired
@@ -28,6 +20,8 @@ public class CommentController {
 
 	@Autowired
 	UserRepository userRepository;
+	
+	// The getting of comments is handled by the forum controller.
 
 	/**
 	 * Convert a comment entity to a comment DTO.
