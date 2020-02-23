971d510b0d91c86cee69d8df76b222a971e76a10
Mat Bis <mathieu.bissonnette2@mail.mcgill.ca>
2020-02-23 16:38:03 -0500

    Add a missing importation to repair the build.

--- PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/controller/ForumController.java
+++ PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/controller/ForumController.java
@@ -9,13 +9,7 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import ca.mcgill.ecse321.petshelter.dto.CommentDTO;
 import ca.mcgill.ecse321.petshelter.dto.ForumDTO;
