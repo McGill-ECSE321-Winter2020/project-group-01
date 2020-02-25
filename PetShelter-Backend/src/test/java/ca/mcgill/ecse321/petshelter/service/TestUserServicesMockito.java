e0d84e1b170556bb0c05d16020fd0a76526f66e7
Ding Ma <43629633+ding-ma@users.noreply.github.com>
2020-02-24 19:11:21 -0500

    fixed according to teacher's specs

--- PetShelter-Backend/src/test/java/ca/mcgill/ecse321/petshelter/service/TestUserServicesMockito.java
+++ PetShelter-Backend/src/test/java/ca/mcgill/ecse321/petshelter/service/TestUserServicesMockito.java
@@ -1,48 +1,30 @@
 package ca.mcgill.ecse321.petshelter.service;
 
+import ca.mcgill.ecse321.petshelter.dto.UserDTO;
+import ca.mcgill.ecse321.petshelter.model.User;
+import ca.mcgill.ecse321.petshelter.model.UserType;
+import ca.mcgill.ecse321.petshelter.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
+
 import static ca.mcgill.ecse321.petshelter.model.UserType.USER;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.lenient;
-import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.mockito.stubbing.Answer;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import ca.mcgill.ecse321.petshelter.dto.UserDTO;
-import ca.mcgill.ecse321.petshelter.model.User;
-import ca.mcgill.ecse321.petshelter.model.UserType;
-import ca.mcgill.ecse321.petshelter.repository.UserRepository;
-
-@RunWith(MockitoJUnitRunner.class)
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class TestUserServicesMockito {
     
     @Mock
     private UserRepository userRepository;
-    
-    @Mock
-	private PasswordEncoder passwordEncoder;
-
-	@Mock
-	private JWTTokenProvider jwtTokenProvider;
-
-	@Mock
-	private EmailingService emailingService;
 
     @InjectMocks
     private UserService userService;
@@ -53,23 +35,23 @@ public class TestUserServicesMockito {
     @BeforeEach
     public void setMockOutput(){
     	MockitoAnnotations.initMocks(this);
-        when(userRepository.findUserByUserName(anyString())).thenAnswer(
-                (InvocationOnMock invocation)->{
-                    if(invocation.getArgument(0).equals(USER_KEY)){
+        lenient().when(userRepository.findUserByUserName(anyString())).thenAnswer(
+                (InvocationOnMock invocation) -> {
+                    if (invocation.getArgument(0).equals(USER_KEY)) {
                         User user = new User();
                         user.setUserName(USER_KEY);
                         return user;
-                    } else{
+                    } else {
                         return null;
                     }
                 });
-        when(userRepository.findUserByEmail(anyString())).thenAnswer(
-                (InvocationOnMock invocation)->{
-                    if(invocation.getArgument(0).equals(USER_KEY2)){
+        lenient().when(userRepository.findUserByEmail(anyString())).thenAnswer(
+                (InvocationOnMock invocation) -> {
+                    if (invocation.getArgument(0).equals(USER_KEY2)) {
                         User user = new User();
                         user.setEmail(USER_KEY2);
                         return user;
-                    } else{
+                    } else {
                         return null;
                     }
                 });
