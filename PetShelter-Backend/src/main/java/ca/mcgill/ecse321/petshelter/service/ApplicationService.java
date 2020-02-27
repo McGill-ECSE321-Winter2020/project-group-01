cc6b97231341d05c9b6b342068f6771a713cbeba
chris <christopher.boustros@mail.mcgill.ca>
2020-02-27 18:10:36 -0500

    Revert "Merge pull request #111 from McGill-ECSE321-Winter2020/AdoptionApplication-development"
    This reverts commit 65b8714ad1275e9ec6bd75c47b997c93ca9c9dcc, reversing
    changes made to 675438a9dad02cd4a552ff57a1da724bd9aaf661.

--- PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/service/ApplicationService.java
+++ PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/service/ApplicationService.java
@@ -8,14 +8,11 @@ import ca.mcgill.ecse321.petshelter.repository.AdvertisementRepository;
 import ca.mcgill.ecse321.petshelter.repository.ApplicationRepository;
 import ca.mcgill.ecse321.petshelter.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class ApplicationService {
@@ -35,47 +32,16 @@ public class ApplicationService {
     }
     
     @Transactional
-    public List<AdoptionApplication> getAllAcceptedApplications() {
-    	List<AdoptionApplication> allApplications = toList(applicationRepository.findAll());
-    	List<AdoptionApplication> acceptedApplications = new ArrayList<AdoptionApplication>();
-    	
-    	for (AdoptionApplication a : allApplications) {
-    		if (a.isIsAccepted()) {
-    			acceptedApplications.add(a);
-    		}
-    	}
-    	
-    	return acceptedApplications;
-    }
-    
-    @Transactional
-    public List<AdoptionApplication> getAllUnacceptedApplications() {
-    	List<AdoptionApplication> allApplications = toList(applicationRepository.findAll());
-    	List<AdoptionApplication> unacceptedApplications = new ArrayList<AdoptionApplication>();
-    	
-    	for (AdoptionApplication a : allApplications) {
-    		if (! a.isIsAccepted()) {
-    			unacceptedApplications.add(a);
-    		}
-    	}
-    	
-    	return unacceptedApplications;
-    }
-    
-    @Transactional
     public AdoptionApplication getApplication(String applicant, Advertisement advertisement) {
-    	return applicationRepository.findApplicationByUserUserNameAndAdvertisement(applicant, advertisement);
+        return applicationRepository.findApplicationByUserUserNameAndAdvertisement(applicant, advertisement);
     }
     
-    @Transactional
-    public AdoptionApplication getApplication(ApplicationDTO applicationDTO) {
-        return applicationRepository.findApplicationByUserUserNameAndAdvertisement(applicationDTO.getUsername(), advertisementRepository.findAdvertisementByTitle(applicationDTO.getAdvertisementTitle()));
-    }
     
     @Transactional
-    public List<AdoptionApplication> getAllUserApplications(User user) {
-        return toList(applicationRepository.findApplicationsByUser(user));
+    public List<AdoptionApplication> getAllUserApplications(User name) {
+        return toList(applicationRepository.findApplicationsByUser(name));
     }
+    
     
     //From tutorial
     private <T> List<T> toList(Iterable<T> iterable) {
@@ -110,80 +76,4 @@ public class ApplicationService {
         applicationRepository.save(application);
         return application;
     }
-    
-    @Transactional
-    public ResponseEntity<?> deleteApplication(ApplicationDTO applicationDTO) {
-    	AdoptionApplication application = getApplication(applicationDTO);
-    	
-    	try {
-    		applicationRepository.deleteById(application.getId());
-    	} catch (RuntimeException e) {
-    		return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-    	}
-    	
-    	return new ResponseEntity<>(HttpStatus.OK);
-    }
-    
-    @Transactional
-    public void acceptApplication(ApplicationDTO applicationDTO) {
-    	
-    	if (applicationDTO == null) {
-    		throw new ApplicationException("Application DTO cannot be null!");
-    	}
-    	
-    	AdoptionApplication application = getApplication(applicationDTO);
-    	
-    	if (application == null) {
-    		throw new ApplicationException("Application not found!");
-    	}
-    	
-    	if (application.isIsAccepted() == true) {
-    		throw new ApplicationException("Application is already accepted!");
-    	}
-    	
-    	application.setIsAccepted(true);
-    	applicationDTO.setIsAccepted(true);
-    	applicationRepository.save(application);
-    }
-    
-    @Transactional
-    public void rejectApplication(ApplicationDTO applicationDTO) {
-    	if (applicationDTO == null) {
-    		throw new ApplicationException("Application DTO cannot be null!");
-    	}
-    	
-    	AdoptionApplication application = getApplication(applicationDTO);
-    	
-    	if (application == null) {
-    		throw new ApplicationException("Application not found!");
-    	}
-    	
-    	if (application.isIsAccepted() == false) {
-    		throw new ApplicationException("Application is already rejected!");
-    	}
-    	
-    	application.setIsAccepted(false);
-    	applicationDTO.setIsAccepted(false);
-    	applicationRepository.save(application);
-    }
-    
-    @Transactional
-    public void updateApplicationDescription(ApplicationDTO applicationDTO, String newDescription) {
-    	if (applicationDTO == null) {
-    		throw new ApplicationException("Application DTO cannot be null!");
-    	}
-    	
-    	AdoptionApplication application = getApplication(applicationDTO);
-    	
-    	if (application == null) {
-    		throw new ApplicationException("Application not found!");
-    	}
-    	
-    	if (application.getDescription() == newDescription) {
-    		throw new ApplicationException("Description is the same!");
-    	}
-    	
-    	application.setDescription(newDescription);
-    }
-    
 }
