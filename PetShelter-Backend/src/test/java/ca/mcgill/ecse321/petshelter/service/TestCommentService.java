4738b4edd7348e8a64f5762374cc920dd44317bb
Mat Bis <mathieu.bissonnette2@mail.mcgill.ca>
2020-02-28 13:06:02 -0500

    Use jUnit5 instead of jUnit4.

--- PetShelter-Backend/src/test/java/ca/mcgill/ecse321/petshelter/service/TestCommentService.java
+++ PetShelter-Backend/src/test/java/ca/mcgill/ecse321/petshelter/service/TestCommentService.java
@@ -7,9 +7,9 @@ import ca.mcgill.ecse321.petshelter.repository.CommentRepository;
 import ca.mcgill.ecse321.petshelter.repository.ForumRepository;
 import ca.mcgill.ecse321.petshelter.repository.UserRepository;
 import ca.mcgill.ecse321.petshelter.service.exception.CommentException;
-import org.junit.Assert;
-import org.junit.Test;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
