1289005eb596e99b7a554ba24361f3d7083ee75f
Louis <louisbarrettevanasse@yahoo.ca>
2020-02-29 17:37:12 -0500

    Update ApplicationController.java

--- PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/controller/ApplicationController.java
+++ PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/controller/ApplicationController.java
@@ -2,6 +2,7 @@ package ca.mcgill.ecse321.petshelter.controller;
 
 import ca.mcgill.ecse321.petshelter.dto.ApplicationDTO;
 import ca.mcgill.ecse321.petshelter.model.AdoptionApplication;
+import ca.mcgill.ecse321.petshelter.model.User;
 import ca.mcgill.ecse321.petshelter.repository.UserRepository;
 import ca.mcgill.ecse321.petshelter.service.ApplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,33 +16,42 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/api/application")
 public class ApplicationController {
-	
+
 	@Autowired
 	private ApplicationService applicationService;
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	/**
 	 * @return the list of all applications
 	 */
 	@GetMapping("/all")
-	public ResponseEntity<?> getAllApplications() {
-		return new ResponseEntity<>(
-				applicationService.getAllApplications().stream().map(this::convertToDto).collect(Collectors.toList()),
-				HttpStatus.OK);
+	public ResponseEntity<?> getAllApplications(@RequestHeader String token) {
+		User requester = userRepository.findUserByApiToken(token);
+		if (requester != null)
+			return new ResponseEntity<>(applicationService.getAllApplications().stream().map(this::convertToDto)
+					.collect(Collectors.toList()), HttpStatus.OK);
+		else
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 	}
-	
+
 	/**
 	 * @param user user targeted
 	 * @return that user's applications
 	 */
 	@GetMapping("/{user}")
-	public ResponseEntity<?> getUserApplication(@PathVariable String user) {
-		return new ResponseEntity<>(applicationService.getAllUserApplications(userRepository.findUserByUserName(user))
-				.stream().map(this::convertToDto).collect(Collectors.toList()), HttpStatus.OK);
+	public ResponseEntity<?> getUserApplication(@PathVariable String user, @RequestHeader String token) {
+		User requester = userRepository.findUserByApiToken(token);
+		if (requester != null) {
+			return new ResponseEntity<>(
+					applicationService.getAllUserApplications(userRepository.findUserByUserName(user)).stream()
+							.map(this::convertToDto).collect(Collectors.toList()),
+					HttpStatus.OK);
+		} else
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 	}
-	
+
 	/**
 	 * Creates an application with the DTO
 	 *
@@ -49,19 +59,24 @@ public class ApplicationController {
 	 * @return check if all the fields are good
 	 */
 	@PostMapping()
-	public ResponseEntity<?> createApplication(@RequestBody ApplicationDTO applicationDTO) {
-		AdoptionApplication application = applicationService.createApplication(applicationDTO);
-		try {
-			applicationDTO.setDescription(application.getDescription());
-			applicationDTO.setUsername(application.getUser().getUserName());
-			applicationDTO.setAdvertisementTitle(application.getAdvertisement().getTitle());
-			applicationDTO.setIsAccepted(application.isIsAccepted());
-			return new ResponseEntity<>(applicationDTO, HttpStatus.OK);
-		} catch (IllegalArgumentException e) {
-			return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-		}
+	public ResponseEntity<?> createApplication(@RequestBody ApplicationDTO applicationDTO,
+			@RequestHeader String token) {
+		User requester = userRepository.findUserByApiToken(token);
+		if (requester != null) {
+			AdoptionApplication application = applicationService.createApplication(applicationDTO);
+			try {
+				applicationDTO.setDescription(application.getDescription());
+				applicationDTO.setUsername(application.getUser().getUserName());
+				applicationDTO.setAdvertisementTitle(application.getAdvertisement().getTitle());
+				applicationDTO.setIsAccepted(application.isIsAccepted());
+				return new ResponseEntity<>(applicationDTO, HttpStatus.OK);
+			} catch (IllegalArgumentException e) {
+				return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+			}
+		} else
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 	}
-	
+
 	/**
 	 * Converts Application to ApplicationDTO
 	 *
@@ -74,7 +89,7 @@ public class ApplicationController {
 		applicationDTO.setUsername(application.getUser().getUserName());
 		applicationDTO.setAdvertisementTitle(application.getAdvertisement().getTitle());
 		applicationDTO.setIsAccepted(application.isIsAccepted());
-		
+
 		return applicationDTO;
 	}
 }
