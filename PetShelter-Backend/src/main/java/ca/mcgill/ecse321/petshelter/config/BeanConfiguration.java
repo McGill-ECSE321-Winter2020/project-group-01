1c7f582ee0990905bd47517479a983234a068f7c
Louis <louisbarrettevanasse@yahoo.ca>
2020-03-01 18:19:22 -0500

    Revert "swagger"
    This reverts commit d00fe9f7a16d1a71bc46ea0b3e79398d3529eb43.

--- PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/config/BeanConfiguration.java
+++ PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/config/BeanConfiguration.java
@@ -19,16 +19,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 @Configuration
 @ConfigurationProperties
 @EnableScheduling
-@EnableSwagger2
 /**
  * Class that configures the beans used throughout the application.
  * 
@@ -92,14 +85,6 @@ public class BeanConfiguration implements WebMvcConfigurer {
 	public AsyncTaskExecutor asyncTaskExecutor() {
 		return new SimpleAsyncTaskExecutor("async");
 	}
-	@Bean
-    public Docket api() { 
-        return new Docket(DocumentationType.SWAGGER_2)  
-          .select()                                  
-          .apis(RequestHandlerSelectors.any())              
-          .paths(PathSelectors.any())                          
-          .build();                                           
-    }
 
 	/**
 	 * Configures @Scheduled tasks
