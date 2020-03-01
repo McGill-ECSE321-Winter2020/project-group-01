cb7a21beb9a4579efafa94513f8ffdf405438aea
Louis <louisbarrettevanasse@yahoo.ca>
2020-03-01 05:24:12 -0500

    pet controller

--- PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/controller/PetController.java
+++ PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/controller/PetController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,85 +23,100 @@ import ca.mcgill.ecse321.petshelter.service.PetService;
 @RestController
 @RequestMapping("/api/pet")
 public class PetController {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private PetService petService;
-	
-	//TODO
-	// NEEDS  TO RETURN A DTO
+
+	// TODO
+	// NEEDS TO RETURN A DTO
 	@GetMapping("/{id}")
-	public ResponseEntity<?> getPet(@RequestHeader String token, @PathVariable long id){
+	public ResponseEntity<?> getPet(@RequestHeader String token, @PathVariable long id) {
 		User requester = userRepository.findUserByApiToken(token);
-		if(requester != null) {
-			return new ResponseEntity<>(petService.getPet(id),HttpStatus.OK);
+		if (requester != null) {
+			return new ResponseEntity<>(petService.getPet(id), HttpStatus.OK);
 		}
 		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 	}
-	
-	//TODO NEEDS TO RETURN LIST OF DTOS
+
+	// TODO NEEDS TO RETURN LIST OF DTOS
 	@GetMapping("/all")
-	public ResponseEntity<?> getAllPets(@RequestHeader String token){
+	public ResponseEntity<?> getAllPets(@RequestHeader String token) {
 		User requester = userRepository.findUserByApiToken(token);
-		if(requester != null) {
-			return new ResponseEntity<>(petService.getAllPets(),HttpStatus.OK);
+		if (requester != null) {
+			return new ResponseEntity<>(petService.getAllPets(), HttpStatus.OK);
 		}
 		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 	}
-	
-	//TODO NEEDS TO RETURN DTOS
+
+	// TODO NEEDS TO RETURN DTOS
 	@GetMapping("/all/{user}")
-	public ResponseEntity<?> getAllPetsFromUser(@RequestHeader String token, @PathVariable String user){
+	public ResponseEntity<?> getAllPetsFromUser(@RequestHeader String token, @PathVariable String user) {
 		User requester = userRepository.findUserByApiToken(token);
-		if(requester != null) {
-			return new ResponseEntity<>(petService.getPetsByUser(user),HttpStatus.OK);
+		if (requester != null) {
+			return new ResponseEntity<>(petService.getPetsByUser(user), HttpStatus.OK);
 		}
 		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 	}
-	
-	//TODO NEEDS TO RETURN DTOS
+
+	// TODO NEEDS TO RETURN DTOS
 	@GetMapping("/all/{advertisement}")
-	public ResponseEntity<?> getAllPetsFromAdvertisement(@RequestHeader String token, @PathVariable long advertisement){
+	public ResponseEntity<?> getAllPetsFromAdvertisement(@RequestHeader String token,
+			@PathVariable long advertisement) {
 		User requester = userRepository.findUserByApiToken(token);
-		if(requester != null) {
-			return new ResponseEntity<>(petService.getPetsByAdvertisement(advertisement),HttpStatus.OK);
+		if (requester != null) {
+			return new ResponseEntity<>(petService.getPetsByAdvertisement(advertisement), HttpStatus.OK);
 		}
 		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 	}
-	
-	//The service method for delete is wrong, so icannot delete correctly here
-//	@DeleteMapping("/{id}")
-//	public ResponseEntity<?> deletePet(@RequestHeader String token, @PathVariable long id){
-//		User requester = userRepository.findUserByApiToken(token);
-//		if(requester != null) {
-//			petService.deletePet(id);
-//			return new ResponseEntity<>(HttpStatus.OK);
-//		}
-//		else {
-//			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-//		}
-//	}
-	
-	//TODO
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deletePet(@RequestHeader String token, @PathVariable long id) {
+		User requester = userRepository.findUserByApiToken(token);
+		if (requester != null) {
+			petService.deletePet(id, requester.getUserName());
+			return new ResponseEntity<>(HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+	}
+
+	/**
+	 * Changes the ownership of a pet
+	 * 
+	 * @param token
+	 * @param id
+	 * @param pet
+	 * @return
+	 */
 	@PutMapping("/{id}")
-	public ResponseEntity<?> updatePet(@RequestHeader String token, @PathVariable long id){
+	public ResponseEntity<?> updatePet(@RequestHeader String token, @PathVariable long id, @RequestBody PetDTO pet) {
 		User requester = userRepository.findUserByApiToken(token);
-		return null;
+		if (requester != null) {
+			PetDTO pet2 = petService.editPet(pet);
+			return new ResponseEntity<>(pet2, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
 	}
-	
+
+	/**
+	 * Creates a pet
+	 * 
+	 * @param token
+	 * @param pet
+	 * @return
+	 */
 	@PostMapping()
-	public ResponseEntity<?> createPet(@RequestHeader String token, @RequestBody PetDTO pet){
+	public ResponseEntity<?> createPet(@RequestHeader String token, @RequestBody PetDTO pet) {
 		User requester = userRepository.findUserByApiToken(token);
-		return null;
+		if (requester != null && requester.getUserName().equals(pet.getUserName())) {
+			PetDTO createdPet = petService.createPet(pet);
+			return new ResponseEntity<>(createdPet, HttpStatus.CREATED);
+		} else {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
 	}
-	
-	@GetMapping("/donate/{id}")
-	public ResponseEntity<?> createPet(@RequestHeader String token, @PathVariable long id){
-		User requester = userRepository.findUserByApiToken(token);
-		return null;
-	}
-	
-	//METHODS LEFT TO DO: Delete pet, update pet, give pet to shelter, create pet
 }
