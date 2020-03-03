5df580b09149d0f1a9656f70a1df338f9c357bb3
Louis <louisbarrettevanasse@yahoo.ca>
2020-03-02 21:22:17 -0500

    navbar

--- petshelter-frontend/src/App.tsx
+++ petshelter-frontend/src/App.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import logo from './default.png';
+import profile from './profile.png';
 import './App.css';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
+// The landing page of the website.
 function App() {
-  return (
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+        <Navbar bg="dark">
+            <Navbar.Brand href="#home">
+                <img
+                    src={profile}
+                    width="60"
+                    height="60"
+                    className="d-inline-block align-top"
+                    alt="logo"
+                />
+            </Navbar.Brand>
+            <Navbar.Collapse className="justify-content-end">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#register">Register</Nav.Link>
+                <Nav.Link href="#login">Login</Nav.Link>
+                <Nav.Link href="#donate">Donate</Nav.Link>
+            </Navbar.Collapse>
+        </Navbar>
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+        </header>
     </div>
   );
 }
