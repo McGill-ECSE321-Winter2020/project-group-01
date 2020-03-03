2ed0e4d4994b0c293a9719752b49946e93d2f04c
Louis <louisbarrettevanasse@yahoo.ca>
2020-03-02 22:40:14 -0500

    landing page done

--- petshelter-frontend/src/App.tsx
+++ petshelter-frontend/src/App.tsx
@@ -4,6 +4,10 @@ import profile from './profile.png';
 import './App.css';
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
+import {MdPersonAdd} from 'react-icons/md';
+import {MdPersonPin} from 'react-icons/md';
+import {MdMonetizationOn} from 'react-icons/md';
+import { IconContext } from "react-icons";
 
 // The landing page of the website.
 function App() {
@@ -11,19 +15,37 @@ function App() {
     <div className="App">
         <Navbar bg="dark">
             <Navbar.Brand href="#home">
+                <span>
                 <img
                     src={profile}
                     width="60"
                     height="60"
                     className="d-inline-block align-top"
                     alt="logo"
-                />
+                /> Pet Pawlace
+                </span>
             </Navbar.Brand>
             <Navbar.Collapse className="justify-content-end">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#register">Register</Nav.Link>
-                <Nav.Link href="#login">Login</Nav.Link>
-                <Nav.Link href="#donate">Donate</Nav.Link>
+                <IconContext.Provider value={{ color: "white", className: "global-class-name", size:"2em" }}>
+                <Nav.Link href="#register">
+                    <MdPersonAdd></MdPersonAdd>
+                    <div className="bg">
+                        Register
+                    </div>
+                </Nav.Link>
+                <Nav.Link href="#login">
+                    <MdPersonPin></MdPersonPin>
+                    <div className="bg">
+                        Login
+                    </div>
+                </Nav.Link>
+                <Nav.Link href="#donate">
+                    <MdMonetizationOn></MdMonetizationOn>
+                    <div className="bg">
+                        Donate
+                    </div>
+                </Nav.Link>
+                </IconContext.Provider>
             </Navbar.Collapse>
         </Navbar>
         <header className="App-header">
