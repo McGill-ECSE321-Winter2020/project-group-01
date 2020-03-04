70197baf038d8fb2351442f8cb66e083a50b9ad3
Louis <louisbarrettevanasse@yahoo.ca>
2020-03-04 03:07:18 -0500

    register and sign in

--- petshelter-frontend/src/App.tsx
+++ petshelter-frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './default.png';
 import profile from './profile.png';
 import './App.css';
@@ -8,52 +8,78 @@ import {MdPersonAdd} from 'react-icons/md';
 import {MdPersonPin} from 'react-icons/md';
 import {MdMonetizationOn} from 'react-icons/md';
 import { IconContext } from "react-icons";
+import SignUp from './Components/SignUp'
+import Register from './Components/Register'
 
 
 // The landing page of the website.
-function App() {
-    return (
-    <div className="App">
-        <Navbar bg="dark">
-            <Navbar.Brand href="#home">
-                <span>
-                <img
-                    src={profile}
-                    width="60"
-                    height="60"
-                    className="d-inline-block align-top"
-                    alt="logo"
-                /> Pet Pawlace
-                </span>
-            </Navbar.Brand>
-            <Navbar.Collapse className="justify-content-end">
-                <IconContext.Provider value={{ color: "white", className: "global-class-name", size:"2em" }}>
-                <Nav.Link href="#register">
-                    <MdPersonAdd></MdPersonAdd>
-                    <div className="bg">
-                        Register
-                    </div>
-                </Nav.Link>
-                <Nav.Link href="#login">
-                    <MdPersonPin></MdPersonPin>
-                    <div className="bg">
-                        Login
-                    </div>
-                </Nav.Link>
-                <Nav.Link href="#donate">
-                    <MdMonetizationOn></MdMonetizationOn>
-                    <div className="bg">
-                        Donate
-                    </div>
-                </Nav.Link>
-                </IconContext.Provider>
-            </Navbar.Collapse>
-        </Navbar>
-        <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-        </header>
-    </div>
-  );
+interface IProps {
+}
+
+interface IState {
+    donateRegisterLoginHome?: string;
+}
+class App extends Component<IProps, IState>{
+    constructor(props:IProps) {
+        super(props);
+        this.state = {
+            donateRegisterLoginHome: 'Home'
+        };
+    }
+    render() {
+        return (
+            <div className="App">
+                <Navbar bg="dark" expand={true} collapseOnSelect={false}>
+                    <Navbar.Brand href="#home" onClick={() => this.changeState('Home')}>
+                        <span>
+                            <img
+                                src={profile}
+                                width="60"
+                                height="60"
+                                className="d-inline-block align-top"
+                                alt="logo"
+                            /> Pet Pawlace
+                        </span>
+                    </Navbar.Brand>
+                    <Navbar.Collapse className="justify-content-end">
+                        <IconContext.Provider value={{ color: "white", className: "global-class-name", size:"2em" }}>
+                            <Nav.Link className="a2" href="#register" onClick={() => this.changeState('Register')}>
+                                <MdPersonAdd></MdPersonAdd>
+                                <div className="bg">
+                                    Register
+                                </div>
+                            </Nav.Link>
+                            <Nav.Link href="#login" className="a2" onClick={() => this.changeState('Login')}>
+                                <MdPersonPin></MdPersonPin>
+                                <div className="bg">
+                                    Login
+                                </div>
+                            </Nav.Link>
+                            <Nav.Link href="#donate" className="a2" onClick={() => this.changeState('Donate')}>
+                                <MdMonetizationOn></MdMonetizationOn>
+                                <div className="bg">
+                                    Donate
+                                </div>
+                            </Nav.Link>
+                        </IconContext.Provider>
+                    </Navbar.Collapse>
+                </Navbar>
+                <header className="App-header">
+                    {this.state.donateRegisterLoginHome === 'Home' && <img src={logo} className="App-logo" alt="logo" />}
+                    {this.state.donateRegisterLoginHome === 'Register' && <Register/>}
+                    {this.state.donateRegisterLoginHome === 'Login' && <SignUp/>}
+                    {this.state.donateRegisterLoginHome === 'Donate' && <img src={logo} className="App-logo" alt="logo" />}
+                </header>
+
+            </div>
+        );
+    }
+
+    changeState(state: string){
+        this.setState({
+            donateRegisterLoginHome: state
+        });
+    }
 }
 
 export default App;
