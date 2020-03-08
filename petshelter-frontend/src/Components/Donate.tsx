22bde3b5402122cd3beae72bbd0b441712bd2991
Ding Ma <ding.ma@mail.mcgill.ca>
2020-03-08 14:15:08 -0400

    removed unsused imports

--- petshelter-frontend/src/Components/Donate.tsx
+++ petshelter-frontend/src/Components/Donate.tsx
@@ -7,8 +7,6 @@ import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import Box from "@material-ui/core/Box";
-import {bool} from "aws-sdk/clients/signer";
-import ThankYou from "./ThankYou";
 
 interface IProps {
     isHome: boolean
